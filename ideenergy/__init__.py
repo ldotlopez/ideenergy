831c3d9c3e9a661aba0abda6d64893d903bcf48c
Luis López <ldotlopez@gmail.com>
2022-02-18 17:03:43 +0100

    Move __doc__

--- ideenergy/__init__.py
+++ ideenergy/__init__.py
@@ -66,3 +66,91 @@ __all__ = [
     "get_session",
     "sanitize_address",
 ]
+
+__doc__ = """
+    # Returned JSON payloads for login phase:
+
+    scenario: Valid credentials.
+    response code: 200
+    response json:
+    {
+        "redirect": "informacion-del-contrato",
+        "zona": "B",
+        "success": "true",
+        "idioma": "ES",
+        "uCcr": "",
+    }
+
+    scenario: Invalid credentials.
+    response code: 200
+    response json:
+    {
+        "success": "false",
+        "message": "El usuario o la contraseña que has introducido son incorrectos. Por favor, inténtalo de nuevo.",
+    }
+
+
+    scenario: Several login retries with invalid credentials.
+    response code: 200
+    response json:
+    {
+      "success": "false",
+      "message": "Número de intentos de acceso excedido. Usuario bloqueado temporalmente. Vuelva a intentarlo dentro de 5 minutos."
+    }
+
+    # Retuned JSON payloads for measures (exposed keys: "valLecturaContador", "valMagnitud", "valEstado")
+
+    scenario: valid measure
+    response code: 200
+    response json:
+    {
+        "valMagnitud": "10.31",
+        "valInterruptor": "1",
+        "valEstado": "09",
+        "valLecturaContador": "42290",
+        "codSolicitudTGT": "009016617100"
+    }
+    {
+        "valMagnitud": "10.49",
+        "valInterruptor": "1",
+        "valEstado": "09",
+        "valLecturaContador": "42290",
+        "codSolicitudTGT": "009016672300",
+    }
+
+    # Cookies from server:
+    [{'version': 0,
+      'name': 'JSESSIONID',
+      'value': '^0000[0-9a-zA-Z]{18}_[0-9a-zA-Z]{4}:[0-9a-zA-Z]{9}$',
+      'port': None,
+      'port_specified': False,
+      'domain': 'www.i-de.es',
+      'domain_specified': False,
+      'domain_initial_dot': False,
+      'path': '/',
+      'path_specified': True,
+      'secure': True,
+      'expires': None,
+      'discard': True,
+      'comment': None,
+      'comment_url': None,
+      'rfc2109': False,
+      '_rest': {'HttpOnly': None}},
+     {'version': 0,
+      'name': 'NSC_wt_mc_mbssvo0Y-[0-9]{5}',
+      'value': '^[0-9a-f]{72}$',
+      'port': None,
+      'port_specified': False,
+      'domain': 'www.i-de.es',
+      'domain_specified': False,
+      'domain_initial_dot': False,
+      'path': '/',
+      'path_specified': True,
+      'secure': True,
+      'expires': 1626089796,               # Aprox. 2 minutes
+      'discard': False,
+      'comment': None,
+      'comment_url': None,
+      'rfc2109': False,
+      '_rest': {'httponly': None}}]
+"""
